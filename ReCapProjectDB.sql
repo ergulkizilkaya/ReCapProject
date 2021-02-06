ba4e9d5219010533abb83834d00dc9ad9002f0f8
ergulkizilkaya <77868230+ergulkizilkaya@users.noreply.github.com>
2021-02-06 05:27:03 +0300

    Add files via upload

---
+++ ReCapProjectDB.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Colors(
+	Id int PRIMARY KEY IDENTITY(1,1),
+	[Name] nvarchar(25),
+)
+
+CREATE TABLE Brands(
+	Id int PRIMARY KEY IDENTITY(1,1),
+	[Name] nvarchar(25),
+)
+
+CREATE TABLE Cars(
+	Id int PRIMARY KEY IDENTITY(1,1),
+	BrandId int,
+	ColorId int,
+	DailyPrice decimal,
+	ModelYear int,
+	Descriptions nvarchar(50),
+	FOREIGN KEY (ColorId) REFERENCES Brands(Id),
+	FOREIGN KEY (BrandId) REFERENCES Colors(Id)
+
+)
+
